62ebc8352c9f65245b5e87c35a750e99d7c9b454
William Normand <nowayride@gmail.com>
2014-03-15 23:49:02 -0500

    This implements #24 and is ready for testing

--- Post.rb
+++ Post.rb
@@ -9,21 +9,29 @@ module Post
     if @@redis.hexists(slug, 'title') == false
       return slug
     else
+      # This will later need to implement a much better duplicates handling
       self.slugify(title+'1')
     end
 
   end
 
-  def self.list paginate: 0
-    redis = Redis.new
-    redis.hgetall 1
+  def self.new post
+    slug = self.slugify(post[:title])
+    @@redis.hmset slug, 'date', post[:date], 'title', post[:title], 'content', post[:content]
+    @@redis.rpush 'post::list', slug
+  end
+
+  def self.list paginate: 0, length: 10
+    titles = @@redis.lrange 'post::list', 0+paginate, 9+paginate
+    posts = []
+    titles.each do |title|
+      posts.push @@redis.hgetall title
+    end
+    return posts
   end
 
   def self.find id
-    id
-  end
-
-  def self.new
+    return @@redis.hgetall id
   end
 
 end
