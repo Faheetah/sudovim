a9dd2284b6e9c8863f5500ef935b035d56069e4c
William Normand <nowayride@gmail.com>
2014-03-15 22:57:33 -0500

    Posts passes slugify tests for issue #24

--- Post.rb
+++ Post.rb
@@ -1,15 +1,29 @@
 require 'redis'
 
 module Post
-  # Grab the last 10 entries
-  def self.all
-    'all'
-  end
-  # Paginate based on offset
 
-  #Grab by ID
-  #test
+  @@redis = Redis.new
+
+  def self.slugify title
+    slug = title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+    if @@redis.hexists(slug, 'title') == false
+      return slug
+    else
+      self.slugify(title+'1')
+    end
+
+  end
+
+  def self.list paginate: 0
+    redis = Redis.new
+    redis.hgetall 1
+  end
+
   def self.find id
     id
   end
+
+  def self.new
+  end
+
 end
---
+++ test/routes
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo
+echo
+echo "root"
+curl localhost:3000/
+echo
+echo
+echo "post"
+curl localhost:3000/post-title
+echo
+echo
+echo "search"
+curl localhost:3000/search?q=foo+bar
+echo
+echo
