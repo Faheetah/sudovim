6a76ce54a23bd198c2d5a4b6dfefd6ac62c56e01
William Normand <nowayride@gmail.com>
2014-03-22 01:37:00 -0500

    Updated test/sample with tags

--- test/sample.rb
+++ test/sample.rb
@@ -4,112 +4,134 @@ module Sample
       {
         :title => "First Post",
         :date => "1-1-14",
-        :content => "Ruby is a dynamic, reflective, object-oriented, general-purpose programming language. It was designed and developed in the mid-1990s by Yukihiro 'Matz' Matsumoto in Japan."
+        :content => "Ruby is a dynamic, reflective, object-oriented, general-purpose programming language. It was designed and developed in the mid-1990s by Yukihiro 'Matz' Matsumoto in Japan.",
+        :tags => "bash,python"
       },
       {
         :title => "Second Post",
         :date => "1-2-14",
-        :content => "According to its authors, Ruby was influenced by Perl, Smalltalk, Eiffel, Ada, and Lisp.[9] It supports multiple programming paradigms, including functional, object-oriented, and imperative. It also has a dynamic type system and automatic memory management."
+        :content => "According to its authors, Ruby was influenced by Perl, Smalltalk, Eiffel, Ada, and Lisp.[9] It supports multiple programming paradigms, including functional, object-oriented, and imperative. It also has a dynamic type system and automatic memory management.",
+        :tags => "bash,python,ruby"
       },
       {
         :title => "Third Post",
         :date => "1-3-14",
-        :content => "Ruby was conceived on February 24, 1993. In a 1999 post to the ruby-talk mailing list, Ruby author Yukihiro Matsumoto describes some of his early ideas about the language:[10]"
+        :content => "Ruby was conceived on February 24, 1993. In a 1999 post to the ruby-talk mailing list, Ruby author Yukihiro Matsumoto describes some of his early ideas about the language:[10]",
+        :tags => "linux,bash"
       },
       {
         :title => "Fourth Post",
         :date => "1-4-14",
-        :content => "I was talking with my colleague about the possibility of an object-oriented scripting language. I knew Perl (Perl4, not Perl5), but I didn't like it really, because it had the smell of a toy language (it still has). The object-oriented language seemed very promising. I knew Python then. But I didn't like it, because I didn't think it was a true object-oriented language — OO features appeared to be add-on to the language. As a language maniac and OO fan for 15 years, I really wanted a genuine object-oriented, easy-to-use scripting language. I looked for but couldn't find one. So I decided to make it."
+        :content => "I was talking with my colleague about the possibility of an object-oriented scripting language. I knew Perl (Perl4, not Perl5), but I didn't like it really, because it had the smell of a toy language (it still has). The object-oriented language seemed very promising. I knew Python then. But I didn't like it, because I didn't think it was a true object-oriented language — OO features appeared to be add-on to the language. As a language maniac and OO fan for 15 years, I really wanted a genuine object-oriented, easy-to-use scripting language. I looked for but couldn't find one. So I decided to make it.",
+        :tags => "ruby,c++"
       },
       {
         :title => "Fifth Post",
         :date => "1-5-14",
-        :content => "Matsumoto describes the design of Ruby as being like a simple Lisp language at its core, with an object system like that of Smalltalk, blocks inspired by higher-order functions, and practical utility like that of Perl.[11]"
+        :content => "Matsumoto describes the design of Ruby as being like a simple Lisp language at its core, with an object system like that of Smalltalk, blocks inspired by higher-order functions, and practical utility like that of Perl.[11]",
+        :tags => "windows"
       },
       {
         :title => "Ruby Language",
         :date => "1-6-14",
-        :content => "The name 'Ruby' originated during an online chat session between Matsumoto and Keiju Ishitsuka on February 24, 1993, before any code had been written for the language.[12] Initially two names were proposed: 'Coral' and 'Ruby'. Matsumoto chose the latter in a later e-mail to Ishitsuka.[13] Matsumoto later noted a factor in choosing the name 'Ruby' – it was the birthstone of one of his colleagues.[14][15]"
+        :content => "The name 'Ruby' originated during an online chat session between Matsumoto and Keiju Ishitsuka on February 24, 1993, before any code had been written for the language.[12] Initially two names were proposed: 'Coral' and 'Ruby'. Matsumoto chose the latter in a later e-mail to Ishitsuka.[13] Matsumoto later noted a factor in choosing the name 'Ruby' – it was the birthstone of one of his colleagues.[14][15]",
+        :tags => "activerecord,ruby on rails"
       },
       {
         :title => "Drinking Soda",
         :date => "1-7-14",
-        :content => "The first public release of Ruby 0.95 was announced on Japanese domestic newsgroups on December 21, 1995.[16][17] Subsequently three more versions of Ruby were released in two days.[12] The release coincided with the launch of the Japanese-language ruby-list mailing list, which was the first mailing list for the new language."
+        :content => "The first public release of Ruby 0.95 was announced on Japanese domestic newsgroups on December 21, 1995.[16][17] Subsequently three more versions of Ruby were released in two days.[12] The release coincided with the launch of the Japanese-language ruby-list mailing list, which was the first mailing list for the new language.",
+        :tags => "ajax,javascript"
       },
       {
         :title => "Mouse Movements",
         :date => "1-8-14",
-        :content => "Already present at this stage of development were many of the features familiar in later releases of Ruby, including object-oriented design, classes with inheritance, mixins, iterators, closures, exception handling and garbage collection.[18]"
+        :content => "Already present at this stage of development were many of the features familiar in later releases of Ruby, including object-oriented design, classes with inheritance, mixins, iterators, closures, exception handling and garbage collection.[18]",
+        :tags => "mvc,python"
       },
       {
         :title => "A Glass Of Milk",
         :date => "1-9-14",
-        :content => "Following the release of Ruby 0.95 in 1995, several stable versions of Ruby were released in the following years:\n\n Ruby 1.0: December 25, 1996[12]\n Ruby 1.2: December 1998\n Ruby 1.4: August 1999\n Ruby 1.6: September 2000\n"
+        :content => "Following the release of Ruby 0.95 in 1995, several stable versions of Ruby were released in the following years:\n\n Ruby 1.0: December 25, 1996[12]\n Ruby 1.2: December 1998\n Ruby 1.4: August 1999\n Ruby 1.6: September 2000\n",
+        :tags => "hardware,raid"
       },
       {
         :title => "Surviving Python Bites",
         :date => "1-10-14",
-        :content => "In 1997, the first article about Ruby was published on the Web. In the same year, Matsumoto was hired by netlab.jp to work on Ruby as a full-time developer.[12]"
+        :content => "In 1997, the first article about Ruby was published on the Web. In the same year, Matsumoto was hired by netlab.jp to work on Ruby as a full-time developer.[12]",
+        :tags => "raid,linux"
       },
       {
         :title => "Drink Coasters Prevent Water Marks",
         :date => "1-11-14",
-        :content => "In 1998, the Ruby Application Archive was launched by Matsumoto, along with a simple English-language homepage for Ruby.[12]"
+        :content => "In 1998, the Ruby Application Archive was launched by Matsumoto, along with a simple English-language homepage for Ruby.[12]",
+        :tags => "ruby,gaming"
       },
       {
         :title => "Keys Unlock Doors",
         :date => "1-12-14",
-        :content => "In 1999, the first English language mailing list ruby-talk began, which signaled a growing interest in the language outside of Japan.[19] In this same year, Matsumoto and Keiju Ishitsuka wrote the first book on Ruby, The Object-oriented Scripting Language Ruby (オブジェクト指向スクリプト言語 Ruby), which was published in Japan in October 1999. It would be followed in the early 2000s by around 20 books on Ruby published in Japanese.[12]"
+        :content => "In 1999, the first English language mailing list ruby-talk began, which signaled a growing interest in the language outside of Japan.[19] In this same year, Matsumoto and Keiju Ishitsuka wrote the first book on Ruby, The Object-oriented Scripting Language Ruby (オブジェクト指向スクリプト言語 Ruby), which was published in Japan in October 1999. It would be followed in the early 2000s by around 20 books on Ruby published in Japanese.[12]",
+        :tags => "gaming,c++"
       },
       {
         :title => "Keyboards type letters",
         :date => "1-13-14",
-        :content => "By 2000, Ruby was more popular than Python in Japan.[20] In September 2000, the first English language book Programming Ruby was printed, which was later freely released to the public, further widening the adoption of Ruby amongst English speakers. In early 2002, the English-language ruby-talk mailing list was receiving more messages than the Japanese-language ruby-list, demonstrating Ruby's increasing popularity in the English-speaking world."
+        :content => "By 2000, Ruby was more popular than Python in Japan.[20] In September 2000, the first English language book Programming Ruby was printed, which was later freely released to the public, further widening the adoption of Ruby amongst English speakers. In early 2002, the English-language ruby-talk mailing list was receiving more messages than the Japanese-language ruby-list, demonstrating Ruby's increasing popularity in the English-speaking world.",
+        :tags => "android,java"
       },
       {
         :title => "Typing This Out Is Boring",
         :date => "1-14-14",
-        :content => "Ruby 1.8 was initially released in August 2003, was stable for a long time, and was retired June 2013.[21] Although deprecated, there is still code based on it. Ruby 1.8 is incompatible with Ruby 1.9."
+        :content => "Ruby 1.8 was initially released in August 2003, was stable for a long time, and was retired June 2013.[21] Although deprecated, there is still code based on it. Ruby 1.8 is incompatible with Ruby 1.9.",
+        :tags => "java,hadoop"
       },
       {
         :title => "This Is A Very Long Title And Should Not Be Longer Than This",
         :date => "1-15-14",
-        :content => "Ruby 1.8 has been the subject of several industry standards. The language specifications for Ruby were developed by the Open Standards Promotion Center of the Information-Technology Promotion Agency (a Japanese government agency) for submission to the Japanese Industrial Standards Committee and then to the International Organization for Standardization. It was accepted as a Japanese Industrial Standard (JIS X 3017) in 2011[22] and an international standard (ISO/IEC 30170) in 2012.[23]"
+        :content => "Ruby 1.8 has been the subject of several industry standards. The language specifications for Ruby were developed by the Open Standards Promotion Center of the Information-Technology Promotion Agency (a Japanese government agency) for submission to the Japanese Industrial Standards Committee and then to the International Organization for Standardization. It was accepted as a Japanese Industrial Standard (JIS X 3017) in 2011[22] and an international standard (ISO/IEC 30170) in 2012.[23]",
+        :tags => "linux,networking"
       },
       {
         :title => "Laptop Screen",
         :date => "1-16-14",
-        :content => "Around 2005, interest in the Ruby language surged in tandem with Ruby on Rails, a popular web application framework written in Ruby. Rails is frequently credited with making Ruby 'famous'.[24]"
+        :content => "Around 2005, interest in the Ruby language surged in tandem with Ruby on Rails, a popular web application framework written in Ruby. Rails is frequently credited with making Ruby 'famous'.[24]",
+        :tags => "ruby,linux,unicorn,nginx"
       },
       {
         :title => "Laptop Keyboard Typing",
         :date => "1-17-14",
-        :content => "Ruby 1.9 was released in December 2007. Effective with Ruby 1.9.3, released October 31, 2011,[25] Ruby switched from being dual-licensed under the Ruby License and the GPL to being dual-licensed under the Ruby License and the two-clause BSD license.[26] Adoption of 1.9 was slowed by changes from 1.8 which required many popular third party gems to be rewritten."
+        :content => "Ruby 1.9 was released in December 2007. Effective with Ruby 1.9.3, released October 31, 2011,[25] Ruby switched from being dual-licensed under the Ruby License and the GPL to being dual-licensed under the Ruby License and the two-clause BSD license.[26] Adoption of 1.9 was slowed by changes from 1.8 which required many popular third party gems to be rewritten.",
+        :tags => "nginx,linux"
       },
       {
         :title => "There's A Snake On My Floor",
         :date => "1-18-14",
-        :content => "Ruby 1.9 introduces many significant changes over the 1.8 series.[27] Examples:"
+        :content => "Ruby 1.9 introduces many significant changes over the 1.8 series.[27] Examples:",
+        :tags => "ruby,javascript,coffee script"
       },
       {
         :title => "My Cat Likes Food",
         :date => "1-19-14",
-        :content => "Matsumoto has said that Ruby is designed for programmer productivity and fun, following the principles of good user interface design.[32] At a Google Tech Talk in 2008 Matsumoto further stated, 'I hope to see Ruby help every programmer in the world to be productive, and to enjoy programming, and to be happy. That is the primary purpose of Ruby language.'[33] He stresses that systems design needs to emphasize human, rather than computer, needs:[34]"
+        :content => "Matsumoto has said that Ruby is designed for programmer productivity and fun, following the principles of good user interface design.[32] At a Google Tech Talk in 2008 Matsumoto further stated, 'I hope to see Ruby help every programmer in the world to be productive, and to enjoy programming, and to be happy. That is the primary purpose of Ruby language.'[33] He stresses that systems design needs to emphasize human, rather than computer, needs:[34]",
+        :tags => "haml,javascript"
       },
       {
         :title => "My Cat Likes Cheese Also",
         :date => "1-20-14",
-        :content => " Often people, especially computer engineers, focus on the machines. They think, 'By doing this, the machine will run faster. By doing this, the machine will run more effectively. By doing this, the machine will something something something.' They are focusing on machines. But in fact we need to focus on humans, on how humans care about doing programming or operating the application of the machines. We are the masters. They are the slaves.  "
+        :content => " Often people, especially computer engineers, focus on the machines. They think, 'By doing this, the machine will run faster. By doing this, the machine will run more effectively. By doing this, the machine will something something something.' They are focusing on machines. But in fact we need to focus on humans, on how humans care about doing programming or operating the application of the machines. We are the masters. They are the slaves.  ",
+        :tags => "ruby,bash"
       },
       {
         :title => "Almost finished",
         :date => "1-21-14",
-        :content => "Ruby is said to follow the principle of least astonishment (POLA), meaning that the language should behave in such a way as to minimize confusion for experienced users. Matsumoto has said his primary design goal was to make a language which he himself enjoyed using, by minimizing programmer work and possible confusion. He has said that he had not applied the principle of least surprise to the design of Ruby,[34] but nevertheless the phrase has come to be closely associated with the Ruby programming language. The phrase has itself been a source of surprise, as novice users may take it to mean that Ruby's behaviors try to closely match behaviors familiar from other languages. In a May 2005 discussion on the newsgroup comp.lang.ruby, Matsumoto attempted to distance Ruby from POLA, explaining that because any design choice will be surprising to someone, he uses a personal standard in evaluating surprise. If that personal standard remains consistent, there would be few surprises for those familiar with the standard.[35]"
+        :content => "Ruby is said to follow the principle of least astonishment (POLA), meaning that the language should behave in such a way as to minimize confusion for experienced users. Matsumoto has said his primary design goal was to make a language which he himself enjoyed using, by minimizing programmer work and possible confusion. He has said that he had not applied the principle of least surprise to the design of Ruby,[34] but nevertheless the phrase has come to be closely associated with the Ruby programming language. The phrase has itself been a source of surprise, as novice users may take it to mean that Ruby's behaviors try to closely match behaviors familiar from other languages. In a May 2005 discussion on the newsgroup comp.lang.ruby, Matsumoto attempted to distance Ruby from POLA, explaining that because any design choice will be surprising to someone, he uses a personal standard in evaluating surprise. If that personal standard remains consistent, there would be few surprises for those familiar with the standard.[35]",
+        :tags => "gaming,ruby"
       },
       {
         :title => "Last Post?",
         :date => "1-22-14",
-        :content => " Everyone has an individual background. Someone may come from Python, someone else may come from Perl, and they may be surprised by different aspects of the language. Then they come up to me and say, 'I was surprised by this feature of the language, so Ruby violates the principle of least surprise.' Wait. Wait. The principle of least surprise is not for you only. The principle of least surprise means principle of least my surprise. And it means the principle of least surprise after you learn Ruby very well. For example, I was a C++ programmer before I started designing Ruby. I programmed in C++ exclusively for two or three years. And after two years of C++ programming, it still surprises me.  "
+        :content => " Everyone has an individual background. Someone may come from Python, someone else may come from Perl, and they may be surprised by different aspects of the language. Then they come up to me and say, 'I was surprised by this feature of the language, so Ruby violates the principle of least surprise.' Wait. Wait. The principle of least surprise is not for you only. The principle of least surprise means principle of least my surprise. And it means the principle of least surprise after you learn Ruby very well. For example, I was a C++ programmer before I started designing Ruby. I programmed in C++ exclusively for two or three years. And after two years of C++ programming, it still surprises me.  ",
+        :tags => "gaming,hardware"
       }
     ]
   end
