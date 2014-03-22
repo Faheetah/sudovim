4e27d50d4c423db55f0f4288885f61586d129a4b
William Normand <nowayride@gmail.com>
2014-03-22 03:53:46 -0500

    Modified tables to move tags to new table

--- db/migrate.rb
+++ db/migrate.rb
@@ -2,12 +2,14 @@ require 'sequel'
 
 DB = Sequel.postgres('sudovim', :user => 'sudovim', :password => 'sudovim', :host => 'localhost')
 
+DB.run 'drop table tags'
+DB.run 'drop table posts'
+
 DB.create_table! :posts do
   primary_key :id
   String :title
   String :slug
   String :content, :text => true
-  String :tags
   Date :date
 end
 
@@ -16,3 +18,4 @@ DB.create_table! :tags do
   foreign_key :posts_id, :posts
   String :tag
 end
+
