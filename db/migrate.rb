e43f55cc2ee11930fb450f6df5c63fe0db8ce265
William Normand <nowayride@gmail.com>
2014-03-22 03:20:01 -0500

    Drop table if exists, added tags table

--- db/migrate.rb
+++ db/migrate.rb
@@ -2,11 +2,17 @@ require 'sequel'
 
 DB = Sequel.postgres('sudovim', :user => 'sudovim', :password => 'sudovim', :host => 'localhost')
 
-DB.create_table :posts do
+DB.create_table! :posts do
   primary_key :id
   String :title
   String :slug
   String :content, :text => true
   String :tags
   Date :date
+end
+
+DB.create_table! :tags do
+  primary_key :id
+  foreign_key :posts_id, :posts
+  String :tag
 end
