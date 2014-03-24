88c4c50abcfc3f581c945a1944c904b22c52eb6f
William Normand <nowayride@gmail.com>
2014-03-23 19:01:49 -0500

    Added :tags recursion that resolves #53

--- Search.rb
+++ Search.rb
@@ -3,7 +3,10 @@ module Search
   @@sequel = Sequel.postgres('sudovim', :user => 'sudovim', :password => 'sudovim', :host => 'localhost')
 
   def self.find terms
-    return @@sequel[:posts].full_text_search(:content, terms).all
+    return @@sequel[:posts].full_text_search(:content, terms).all.each do |post|
+      post[:tags] = Tag.find post[:id]
+    end
+    return posts
   end
 
   def self.tags tags
