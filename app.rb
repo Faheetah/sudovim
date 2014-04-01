3d2ac80044ad820d0e9bb1f9abd8ee58c254f542
William Normand <nowayride@gmail.com>
2014-04-01 04:34:01 -0500

    Added new post with authentication, this implements #46

--- app.rb
+++ app.rb
@@ -5,6 +5,8 @@ require './Post'
 require './Tag'
 require './Search'
 
+use Rack::Session::Cookie, :key => 'auth', :domain => 'localhost', :path => '/new', :expire_after => 2592000, :secret => '8rj3289rj9foeawhf98o4haf8493oafhaw8ehufizlhfg78oh8'
+
 before '/*' do
   @paginate = params[:p].to_i
   @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(fenced_code_blocks: true, no_links: true, hard_wrap: true), extensions = {no_intra_emphasis: true, superscript: true, strikethrough: true, prettify: true})
@@ -27,6 +29,43 @@ get '/tag/*' do
   @search = Search.tags(@query).flat_map(&:values)
   @posts = Post.find @search, paginate: @paginate
   erb :index
+end
+
+get '/new' do
+  if session[:login] == 'true'
+    erb :new
+  else
+    erb :login
+  end
+end
+
+post '/new' do
+  if session[:login] == 'true'
+    if params['preview']
+      erb :new
+    elsif params['submit']
+      @post = Post.new title: params[:title], summary: params[:summary], content: params[:content], tags: params[:tags]
+      redirect '/'+@post.to_s
+    end
+  else
+    erb :login
+  end
+end
+
+post '/login' do
+  if params[:username] != '' or params[:password]
+    redirect '/'
+  elsif params[:auth] == (DateTime.now.strftime('%-Y%-j').to_i ** DateTime.now.strftime('%-w%-l').to_i).to_s(16)
+    session[:login] = 'true'
+    redirect '/new'
+  else
+    redirect '/'
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/new'
 end
 
 get '/:id*' do
